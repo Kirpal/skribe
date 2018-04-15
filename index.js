eaa81cd5dd01b5cf4739d35a00bbef4ba4bed897
Kirpal Demian <demian@kirp.al>
2018-04-14 23:51:51 -0400

    Fixed get to post

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ db.once('open', () => {
         res.render(__dirname + '/src/create.pug');
     })
     
-    app.get('/verify', (req, res) => {
+    app.post('/verify', (req, res) => {
         async function verify() {
             const ticket = await client.verifyIdToken({
                 idToken: req.params.id,
